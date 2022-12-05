9f1bdbfcc0a81ada7f945b3cbb446b4d5da9ac98
pasta04 <aldente04@gmail.com>
2022-12-05 18:33:31 +0900

    winter 2022

--- src/createqr.js
+++ src/createqr.js
@@ -1,21 +1,52 @@
-const fs = require('fs');
+const fs = require('fs-extra');
+const path = require("path");
 const QRCode = require('qrcode');
 
 /** QRの画像を生成 */
-const createQr = async() => {
+const createQr = async () => {
   // ファイル読込
   const list = [];
-  for(const filename of ["8_11", "8_12","8_13","8_14","8_15"]) {
-    const json = JSON.parse(fs.readFileSync("data/"+filename+".json").toString());
-    json.map((value)=> value.day = filename);
+  for (const date of ["12月26日", "12月27日", "12月28日", "12月29日", "12月30日", "12月31日"]) {
+    const filename = `mail_${date}`
+    const json = JSON.parse(fs.readFileSync("data/" + filename + ".json").toString());
+    console.log(`${filename} は ${json.length} 件のデータ`);
+
+    // データチェック
+    for (const item of json) {
+      if (!item.day) {
+        console.warn(item);
+        return;
+      }
+      if (!item.mail) {
+        console.warn(item);
+        return;
+      }
+      if (!item.name) {
+        console.warn(item);
+        return;
+      }
+      if (!item.code) {
+        console.warn(item);
+        return;
+      }
+    }
 
     list.push(...json);
   }
+  console.log(`計 ${list.length} 件のQRを出力`);
   console.log(list[0]);
 
   // QR作成
-  for(const item of list) {
-    const qrpath = `data/image/${item.day}/${item.mail.replace("@", "").replace(/\./g, "")}.png`;
+  for (const item of list) {
+    const qrpath = `data/image/${item.day}/${item.code}.png`;
+    if (!fs.existsSync(path.dirname(qrpath))) {
+      fs.mkdirpSync(path.dirname(qrpath));
+    }
+    if (fs.existsSync(qrpath)) {
+      console.warn(`生成済み: ${qrpath}`);
+      continue;
+    }
+
     const text = item.code;
     const option = {
       type: "png",
@@ -23,9 +54,9 @@ const createQr = async() => {
     };
 
     QRCode.toFile(qrpath, text, option, function (err) {
-      if(err){
+      if (err) {
         console.log(`${item.mail} でエラー`);
-        console.log(err);  
+        console.error(err);
       }
     });
   }
--- src/sendmail.js
+++ src/sendmail.js
@@ -14,36 +14,38 @@ const transporter = nodemailer.createTransport({
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 /** メール送る */
-const sendMail = async (mail, date, filepath, code) => {
-  const subject = `RTA in Japan Summer 2022 入場コード(${date})`;
+const sendMail = async (mail, date, filepath, code, dryrun) => {
+  const subject = `RTA in Japan Winter 2022 入場コード(${date})`;
 
-  const text = `RTA in Japan Summer 2022 ${date}の入場に必要なコードを送付いたします。新型コロナウイルスのワクチン接種証明書と併せて会場の受付に提示してください。
+  const text = `RTA in Japan Winter 2022 ${date}の入場が当選しました。
 
-添付画像が表示できない場合は以下のURLにアクセスしてください。
-https://rtain.jp/code/?data=${code}
+  入場に必要なQRコードを送付いたします。
+  新型コロナウイルスのワクチン接種証明書と併せて、会場の受付に提示してください。
+  
+  添付画像が表示できない場合は以下のURLにアクセスしてください。
+  https://rtain.jp/code/?data=${code}
 
 
-また、事前に以下をご確認の上、ご来場頂ますようお願い致します。
-
+また、事前に以下をご確認の上、ご来場頂きますようお願い致します。
 - 入場用リンクの譲渡は厳禁です。
 - 入場には新型コロナウイルスワクチンの接種完了が証明できるものが必要です。PCR検査陰性などは代わりになりません。
 - 入場時に無料でN95マスクを配布します。特別な理由がない限り着用をお願いします。
-- 入場登録に記載した通り、同時に入れる人数は決まっており、必ず会場に入れるとは限りません。
+- 会場に滞在可能な時間は当選日の00:00～23:59となります。日を跨いで滞在する場合は、翌日のコードによる再認証が必要です。
+- 入場登録フォームに記載した通り、同時に入れる人数は決まっており、必ず会場に入れるとは限りません。
 - 会場で荷物は預かれません。持ち物は全て自己責任で管理してください。盗難などの責任も取れません。
 - 会場で食事をできるところはありません。
-- 会場に入った時点で、配信や撮影のカメラにうつる可能性が常にありますのでご承知ください。
-- 会場にゴミ箱はありません。
+- 会場に入った時点で、配信や撮影のカメラに映る可能性が常にありますのでご承知ください。
+- ゴミは各自で持ち帰って下さい。
 - 酒類の持ち込みおよび酒気帯びで入場はできません。
 - 車椅子スペースなど会場内で特別な対応を希望される方は、できるだけ事前に info@rtain.jp までご連絡ください。
-- 会場内や会場周辺での徹夜待機などは近所の迷惑になりますので絶対にやめてください。
+- 会場周辺での徹夜待機などは近所の迷惑になりますので絶対にやめてください。
 - その他の注意事項は https://rtain.jp/audience/ をご確認ください。
 
 ------------------
 RTA in Japan
 `;
-  // console.log(text);
 
-  const result = await transporter.sendMail({
+  const sendObj = {
     from: 'info@rtain.jp',
     to: mail,
     subject: subject,
@@ -55,26 +57,32 @@ RTA in Japan
         path: filepath,
       },
     ],
-  });
+  };
 
-  console.log(`${mail} ${result.messageId}`);
+  fs.appendFileSync("data/senddump.log", JSON.stringify(sendObj, null, "  ") + ",\n");
+  if (!dryrun) {
+    const result = await transporter.sendMail(sendObj);
+    console.log(`${mail} ${result.messageId}`);
+  }
 }
 
-const main = async () => {
-  for(const date of ["8月00日"]) {
-    const dateId = date.replace("月", "_").replace("日", "");
-    const list = JSON.parse(fs.readFileSync(`data/${dateId}.json`).toString());
-  
-    for(const item of list) {
-      console.log(`----------------------- ${item.mail} -----------------------`);
+const main = async (dryrun) => {
+  if (dryrun) console.log("★dryrun mode!★")
 
-      const qrpath = `data/image/${dateId}/${item.mail.replace("@", "").replace(/\./g, "")}.png`;
-      if(!fs.existsSync(qrpath)) {
+  for (const date of ["12月26日", "12月27日", "12月28日", "12月29日", "12月30日", "12月31日"]) {
+    const list = JSON.parse(fs.readFileSync(`data/mail_${date}.json`).toString());
+    console.log(`================= ${date} ==========================`);
+
+    for (const item of list) {
+      console.log(`${item.mail} ${date} ${item.code}`);
+
+      const qrpath = `data/image/${date}/${item.code}.png`;
+      if (!fs.existsSync(qrpath)) {
         console.log(`${qrpath}がありません`);
       } else {
         try {
-          await sendMail(item.mail, date, qrpath, item.code);
-        } catch(e) {
+          await sendMail(item.mail, date, qrpath, item.code, dryrun);
+        } catch (e) {
           console.log(`${mail} でエラーが起きた`);
         }
 
@@ -82,8 +90,8 @@ const main = async () => {
         await sleep(100);
       }
     }
-  }  
+  }
 }
 
 // メール送信
-main();
+main(false);
