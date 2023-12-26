37a7ffef33adf7dba55afcfe181ef0adedad8b26
pasta04 <aldente04@gmail.com>
2023-12-26 09:09:58 +0900

    w2023

--- src/sendmail.js
+++ src/sendmail.js
@@ -14,34 +14,24 @@ const transporter = nodemailer.createTransport({
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 /** メール送る */
-const sendMail = async (mail, date, filepath, code, dryrun) => {
-  const subject = `RTA in Japan Summer 2023 入場コード(${date})`;
+const sendMail = async (mail, dryrun) => {
+  const subject = `RTA in Japan Winter 2023 入場案内`;
 
-  const text = `RTA in Japan Summer 2023 ${date}の入場が当選しました。
+  const text = `RTA in Japan Winter 2023 は本日開催となります。
 
-入場に必要なQRコードを送付いたします。
-新型コロナウイルスのワクチン接種証明書と併せて、会場の受付に提示してください。
-
-添付画像が表示できない場合は以下のURLにアクセスしてください。
-https://rtain.jp/code/?data=${code}
-
-また、事前に以下をご確認の上、ご来場頂きますようお願い致します。
-- この入場コードは1名のみが利用可能で、介助者などの自身の生活をサポートするための人を除いて同伴はできません。
-- 入場用リンクの譲渡は厳禁です。
-- 入場には新型コロナウイルスワクチンの接種完了が証明できるものが必要です。PCR検査陰性などは代わりになりません。
-- 会場では、N95/KF94/KN95に相当するマスクを常に着用する必要があります。特別な理由がない限り会場受付で配布するマスクを着用してください。
-- 会場に滞在可能な時間は当選日の00:00～23:59となります。日を跨いで滞在する場合は、翌日のコードによる再認証が必要です。
-- 18歳未満は22時以降の来場禁止、16歳未満は18時以降の来場禁止（保護者同伴なら22時まで可）となっています。
-- 入場登録フォームに記載した通り、同時に入れる人数は決まっており、必ず会場に入れるとは限りません。
-- 入場に制限を掛けるときは速やかに公式TwitterとDiscordで告知いたしますが、その際に会場の外で列を作らないでください。
-- 会場で荷物は預かれません。持ち物は全て自己責任で管理してください。盗難などの責任も取れません。
-- 会場で食事をできるところはありません。
-- 会場に入った時点で、配信や撮影のカメラに映る可能性が常にありますのでご承知ください。参考: https://rtain.jp/photos/ https://www.youtube.com/playlist?list=PLFvJYuQufMw4pYp1Aru3z1-G69HWUg6tT
-- ゴミは各自で持ち帰って下さい。
-- 酒類の持ち込みおよび酒気帯びで入場はできません。
-- 車椅子スペースなどの対応を希望される方は、事前に info@rtain.jp までご連絡いただけるとスムーズです。
-- 会場周辺での徹夜待機などは近所の迷惑になりますので絶対にやめてください。
-
+観客の入場は 本日12月26日 11時～ となります。
+どうぞふるってお越しください。
+    
+また、マスクの案内について一部変更がございます。
+来場者数に対し十分な数のマスクを用意するのが難しいため、マスク装着のポリシーを変更します。
+  
+会場ではマスクの用意があり無料でお渡しできますが、必ず不織布で口と鼻を覆える、できればN95/KF94に相当するマスクのご用意をそれぞれでお願いします。
+入場時、上記に相当しない方にマスクをお渡しするので必ず装着をお願いします。
+  
+また、来場前にご自身の体調は必ず確認してください。咳やくしゃみが出る、喉や頭が痛いなどひとつでも該当する方は来場しないでください。
+  
+冬の乾燥した時期に長時間大人数が接する場ですので、感染症予防のためご協力をお願いします。
+  
 ------------------
 RTA in Japan
 `;
@@ -50,14 +40,7 @@ RTA in Japan
     from: 'info@rtain.jp',
     to: mail,
     subject: subject,
-    text: text,
-    // 添付ファイル
-    attachments: [
-      {
-        filename: 'code.png',
-        path: filepath,
-      },
-    ],
+    text: text
   };
 
   fs.appendFileSync("data/senddump.log", JSON.stringify(sendObj, null, "  ") + ",\n");
@@ -70,33 +53,25 @@ RTA in Japan
 const main = async (dryrun) => {
   if (dryrun) console.log("★dryrun mode!★")
 
-  for (const date of ["8月10日", "8月11日", "8月12日", "8月13日", "8月14日", "8月15日"]) {
-    const jsonname = `data/mail_${date}.json`;
-    if (!fs.existsSync(jsonname)) {
-      console.error(`JSON file is not found. filename=${jsonname}`);
-      continue;
+  const jsonname = `data/list.txt`;
+  if (!fs.existsSync(jsonname)) {
+    console.error(`file is not found. filename=${jsonname}`);
+    return;
+  }
+  const list = fs.readFileSync(jsonname).toString().split("\n");
+
+  for (const mail of list) {
+    if (!mail) continue;
+    console.log(`${mail}`);
+
+    try {
+      await sendMail(mail, dryrun);
+    } catch (e) {
+      console.log(`${mail} でエラーが起きた`);
+      console.error(e);
     }
-    const list = JSON.parse(fs.readFileSync(jsonname).toString());
-    console.log(`================= ${date} ==========================`);
-
-    for (const item of list) {
-      console.log(`${item.mail} ${date} ${item.code}`);
-
-      const qrpath = `data/image/${date}/${item.code}.png`;
-      if (!fs.existsSync(qrpath)) {
-        console.log(`${qrpath}がありません`);
-      } else {
-        try {
-          await sendMail(item.mail, date, qrpath, item.code, dryrun);
-        } catch (e) {
-          console.log(`${mail} でエラーが起きた`);
-          console.error(e);
-        }
-
-        // 1秒間あたりの送信リミットに引っかからないようにsleep入れる
-        if (!dryrun) await sleep(100);
-      }
-    }
+    // 1秒間あたりの送信リミットに引っかからないようにsleep入れる
+    if (!dryrun) await sleep(100);
   }
 }
 
